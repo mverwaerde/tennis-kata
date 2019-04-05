9b511251d5f200c95208ba269d2d2b89659b39fc
Marine Verwaerde <mverwaerde@octo.com>
2019-04-05 14:38:08 +0200

    WIP1-Extract Functions

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,9 +1,8 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -14,89 +13,110 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+        if (P1point == P2point && P1point < 4) {
+            score = updateScoreBeforeAdvantageWhenEquality(score);
         }
-        if (P1point==P2point && P1point>=3)
+        if (P1point == P2point && P1point >= 3) {
             score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
         }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+
+        if (P1point > 0 && P2point == 0) {
+            score = updateScoreWithPlayer1Winning();
         }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+        if (P2point > 0 && P1point == 0) {
+            score = updateScoreWithPlayer2Winning();
         }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+
+        if (P1point > P2point && P1point < 4) {
+            score = updateScoreBeforeAdvantageWhenPlayer1IsWinning();
         }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
+        if (P2point > P1point && P2point < 4) {
+            score = updateScoreBeforeAdvantageWhenPlayer2IsWinnning();
+        }
+
+        if (P1point > P2point && P2point >= 3) {
             score = "Advantage player1";
         }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
+
+        if (P2point > P1point && P1point >= 3) {
             score = "Advantage player2";
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
+
+        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
+        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
             score = "Win for player2";
         }
+        return score;
+    }
+
+    private String updateScoreBeforeAdvantageWhenEquality(String score) {
+        if (P1point == 0)
+            score = "Love";
+        if (P1point == 1)
+            score = "Fifteen";
+        if (P1point == 2)
+            score = "Thirty";
+        score += "-All";
+        return score;
+    }
+
+    private String updateScoreBeforeAdvantageWhenPlayer2IsWinnning() {
+        String score;
+        if (P2point == 2)
+            P2res = "Thirty";
+        if (P2point == 3)
+            P2res = "Forty";
+        if (P1point == 1)
+            P1res = "Fifteen";
+        if (P1point == 2)
+            P1res = "Thirty";
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private String updateScoreBeforeAdvantageWhenPlayer1IsWinning() {
+        String score;
+        if (P1point == 2)
+            P1res = "Thirty";
+        if (P1point == 3)
+            P1res = "Forty";
+        if (P2point == 1)
+            P2res = "Fifteen";
+        if (P2point == 2)
+            P2res = "Thirty";
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private String updateScoreWithPlayer2Winning() {
+        String score;
+        if (P2point == 1)
+            P2res = "Fifteen";
+        if (P2point == 2)
+            P2res = "Thirty";
+        if (P2point == 3)
+            P2res = "Forty";
+
+        P1res = "Love";
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private String updateScoreWithPlayer1Winning() {
+        String score;
+        if (P1point == 1)
+            P1res = "Fifteen";
+        if (P1point == 2)
+            P1res = "Thirty";
+        if (P1point == 3)
+            P1res = "Forty";
+
+        P2res = "Love";
+        score = P1res + "-" + P2res;
         return score;
     }
 
